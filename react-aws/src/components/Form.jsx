11164a4a35f5419a9ebcfb270d27cdab0cf2272d
ZEEL PATEL <141486033+zeelapatel@users.noreply.github.com>
2024-04-16 11:43:30 -0400

    v1

---
+++ react-aws/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SingleFileUploader from './file-upload/SingleFileUploader.jsx'
+const Form = () => {
+
+    
+  return (
+    <>
+    
+    <SingleFileUploader/>
+    </>
+
+  )
+}
+
+export default Form
---
+++ react-aws/src/components/file-upload/SingleFileUploader.jsx
@@ -0,0 +1,77 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { useState } from "react";
+import { OutlinedInput, TextField, styled } from '@mui/material';
+import axios from "axios";
+
+function SingleFileUploader() {
+  const [file, setFile] = useState(null);
+  const [textInput, setTextInput] = useState('');
+
+  const Input = styled(TextField)`
+    color: red;
+    margin: 10px; 
+  `;
+
+  // Function to upload file to s3
+  const uploadFile = async () => {
+    const S3_BUCKET = "react-file";
+    const REGION = import.meta.env.VITE_AWS_REGION;
+    const UPLOAD_API_ENDPOINT = import.meta.env.VITE_AWS_API_ENDPOINT; // Replace with your API Gateway endpoint
+
+    const s3Client = new S3Client({
+      region: REGION,
+      credentials: {
+        accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+        secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    try {
+      await s3Client.send(new PutObjectCommand(params));
+      console.log("File uploaded successfully.");
+      alert("File uploaded successfully.");
+      
+      const requestBody = {
+        text: textInput,
+        filePath: `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${file.name}`
+      };
+
+      // Here you can send requestBody to your API endpoint
+      await axios.post(UPLOAD_API_ENDPOINT, requestBody);
+
+    } catch (err) {
+      alert("File uploaded booo.");
+      console.error("Error uploading file:", err);
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
+  };
+
+  const handleTextChange = (e) => {
+    setTextInput(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <input type = "text" 
+          id="outlined-basic" 
+          label="Text Input" 
+        />
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={uploadFile}>Upload</button>
+      </div>
+    </div>
+  );
+}
+
+export default SingleFileUploader;
